478894946b73384dcbfdde3f7fb37bd11f683965
Nazeeeh <nazihahmed55@gmail.com>
2021-03-24 09:19:59 +0100

    first commit message

---
+++ src/app/todos/todos.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+import { Todo } from '../model/Todos';
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css']
+})
+export class TodosComponent implements OnInit {
+
+  todos : Todo[];
+  inputTodo:string = "";
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+    this.todos = [
+      {
+        content : 'First todos',
+        completed : false
+      }, 
+      {
+        content : 'Second todos',
+        completed : false
+      }
+    ]
+  }
+
+  toggleDone (id:number) : void{
+    this.todos.map((v, i)=> {
+      if(i == id) v.completed = !v.completed;
+    });
+  }
+
+  deleteTodo (id:number) {
+    this.todos = this.todos.filter((v, i) =>  i !== id);
+
+  }
+
+  addTodo () {
+    if(this.inputTodo === ""){
+      alert("Field can't be empty")
+    } else {
+      this.todos.push({
+        content : this.inputTodo,
+        completed : false
+      });
+
+      this.inputTodo = "";
+    }
+    
+  }
+
+}
